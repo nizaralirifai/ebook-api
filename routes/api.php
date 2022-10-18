0328df48156be49452fb26e97b6cd67a33001153
nizaralirifai <91579596+nizaralirifai@users.noreply.github.com>
2022-10-18 15:55:41 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('me', [Authcontroller::class, 'me']);
+
+// Route::get('books', [Bookcontroler::class, 'index']);
+// Route::get('books/{id}', [Bookcontroler::class, 'show']);
+// Route::post('books', [Bookcontroler::class, 'store']);
+// Route::put('books/{id}', [Bookcontroler::class, 'update']);
+// Route::delete('books/{id}', [Bookcontroler::class, 'destroy']);
+
+Route::resource('book', BookController::class)->except(
+    ['create', 'edit']
+);
+
