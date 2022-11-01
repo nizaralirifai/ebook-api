60573d643d314c3b7b0e8dbdc2755670f88f2726
nizaralirifai <91579596+nizaralirifai@users.noreply.github.com>
2022-11-01 13:42:14 +0700

    Challange Author

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class Authcontroller extends Controller
 {
@@ -12,6 +16,66 @@ class Authcontroller extends Controller
             'Name' => 'Nizar Ali Rifai',
             'Phone' => '0896 7876 5674',
             'Class' => 'XII RPL 5'
+        ];
+    }
+
+    public function register(Request $request){
+        
+        $field = $request -> validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|max:100|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $field['name'],
+            'email' => $field['email'],
+            'password' => bcrypt($field['password'])
+        ]);
+
+        $token = $user->CreateToken('tokenku')->plainTextToken;
+
+        $response =[
+            'user'=>$user,
+            'token'=>$token
+        ];
+
+        return response($response, 201);
+    }
+   
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+    //check email
+    $user = User::where('email', $fields['email'])->first();
+
+    //Check password
+    if (!$user || !Hash::check($fields['password'], $user->password)) {
+        return response([
+            'message' => 'unauthorized'
+        ], 401);
+    }
+
+    $token = $user->createToken('tokenku')->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+
+    return response($response, 201);
+}
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'message' => 'Logged out'
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\Authorcontroller;
-
+use App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,17 +20,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me', [Authcontroller::class, 'me']);
+//public routes
 
-// Route::get('books', [Bookcontroler::class, 'index']);
-// Route::get('books/{id}', [Bookcontroler::class, 'show']);
-// Route::post('books', [Bookcontroler::class, 'store']);
-// Route::put('books/{id}', [Bookcontroler::class, 'update']);
-// Route::delete('books/{id}', [Bookcontroler::class, 'destroy']);
 
-Route::resource('book', BookController::class)->except(
-    ['create', 'edit']
-);
-Route::resource('author', AuthorController::class)->except(
-    ['create', 'edit']
-);
+Route::get('me', [AuthController::class, 'me']);
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{id}', [BookController::class, 'show']);
+Route::get('authors', [AuthorController::class, 'index']);
+Route::get('authors/{id}', [AuthorController::class, 'show']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//protected routes
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('books', BookController::class)->except(
+        ['create','edit','index','show']
+    );
+
+    Route::resource('author', AuthorController::class)->except(
+        ['create','edit','index','show']
+    );
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
